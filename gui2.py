2b8e6b4883e66a58b15a7a1421cb750f4dd57e2a
yahyapqb <yhyyhmldar8@gmail.com>
2026-06-30 06:44:44 -0700

    setgeometry

--- gui2.py
+++ gui2.py
@@ -9,12 +9,10 @@ window.setWindowTitle("first project")
 window.resize( 720 , 500 )
 
 label = QLabel ("hello world" , window)
-
-label.resize(500 , 500)
+label.setGeometry(100 , 10 , 200 , 200)
 
 button = QPushButton("click me" , window)
-button.move(360 , 270)
-button.resize(500 , 100)
+button.setGeometry(340 , 100 , 340 , 100)
 button.clicked.connect(h)
 
 window.show()
