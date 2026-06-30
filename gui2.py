ff327518fb38465ab4cee1722ba286a14852e476
yahyapqb <yhyyhmldar8@gmail.com>
2026-06-30 06:40:05 -0700

    resize

--- gui2.py
+++ gui2.py
@@ -9,10 +9,12 @@ window.setWindowTitle("first project")
 window.resize( 720 , 500 )
 
 label = QLabel ("hello world" , window)
-label.move(360, 250)
+
+label.resize(500 , 500)
 
 button = QPushButton("click me" , window)
 button.move(360 , 270)
+button.resize(500 , 100)
 button.clicked.connect(h)
 
 window.show()
