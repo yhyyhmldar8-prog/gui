7efc4ce7a3d075b3d0044af5931513727f742e56
yahyapqb <yhyyhmldar8@gmail.com>
2026-06-30 06:33:09 -0700

    first change part1

--- gui2.py
+++ gui2.py
@@ -1,1 +1,20 @@
+from PySide6.QtWidgets import *
+def h():
+    label.setText("connected")
 
+app = QApplication([])
+
+window = QWidget()
+window.setWindowTitle("first project")
+window.resize( 720 , 500 )
+
+label = QLabel ("hello world" , window)
+label.move(360, 250)
+
+button = QPushButton("click me" , window)
+button.move(360 , 270)
+button.clicked.connect(h)
+
+window.show()
+
+app.exec()
