575345deb72021952ceb889729a33e808783872b
yahyapqb <yhyyhmldar8@gmail.com>
2026-06-30 06:54:21 -0700

    font

--- gui2.py
+++ gui2.py
@@ -1,15 +1,18 @@
-from PySide6.QtWidgets import *
+from PySide6.QtWidgets import * 
+from PySide6.QtGui import QFont
 def h():
     label.setText("connected")
 
 app = QApplication([])
+font = QFont("Arial", 32)
 
 window = QWidget()
 window.setWindowTitle("first project")
 window.setGeometry( 0 , 0 , 720 , 500 )
 
 label = QLabel ("hello world" , window)
-label.setGeometry(100 , 10 , 200 , 200)
+label.setGeometry(100 , 10 , 300 , 200)
+label.setFont(font)
 
 button = QPushButton("click me" , window)
 button.setGeometry(340 , 100 , 340 , 100)
