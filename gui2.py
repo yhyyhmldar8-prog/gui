b75cc28008cb19c2fc0107adf780bfb51875df87
yahyapqb <yhyyhmldar8@gmail.com>
2026-07-02 12:37:11 -0700

    some thing

--- gui2.py
+++ gui2.py
@@ -1,14 +1,21 @@
 from PySide6.QtWidgets import * 
 from PySide6.QtGui import QFont
+from PySide6.QtCore import Qt
 def h():
     label.setText("connected")
     button.setText("hehehe")
     button.setEnabled(False)
-    label.setVisible(False)
+    label.setEnabled(False)
+    window.setWindowTitle("next")
+    label.setGeometry( 0 ,0 ,200 , 10)
 def g():
-    label.setVisible(True)
+    label.setEnabled(True)
+def z():
+    label.setText("its a joke dont angry")
+    buttom.setStyleSheet(" color: red ; background-color:green")
 app = QApplication([])
 font = QFont("Arial", 10)
+font1 = QFont("Arial" , 15)
 
 window = QWidget()
 window.setWindowTitle("first project")
@@ -25,8 +32,15 @@ button.clicked.connect(h)
 
 button1 = QPushButton("its a joke click again" , window)
 button1.setGeometry( 50 , 100 , 200 , 50)
+button1.setCursor(Qt.CursorShape.ClosedHandCursor)
+button1.setObjectName("dokme")
 button1.clicked.connect(g)
 
+buttom = QPushButton("again test", window)
+buttom.setGeometry( 20, 20 , 100,100 )
+buttom.clicked.connect(z)
+buttom.setFont(font1)
+buttom.setToolTip("if you click this button you will die or get rich")
 window.show()
 
 app.exec()
